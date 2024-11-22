066dea442c2ef41adf2cc961396fedd3f255ac48
kmiibfw <kmiibfw@gmail.com>
2024-11-22 22:35:55 +0900

    [add] managementテーブルのモデル作成

---
+++ src/main/java/com/example/team_management/models/management/Management.java
@@ -0,0 +1,17 @@
+package com.example.team_management.models.management;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Management {
+    private int management_id;
+    private int user_id;
+    private String subject;
+    private String link;
+    private int status;
+    private LocalDateTime create_at;
+    private LocalDateTime update_at;
+    private LocalDateTime delete_at;
+}
---
+++ src/main/java/com/example/team_management/models/management/ManagementDao.java
@@ -0,0 +1,27 @@
+package com.example.team_management.models.management;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+public interface ManagementDao {
+
+    // データ追加
+    public int addManagementData(Management manaegment) throws DataAccessException;
+
+    // 全完了データ取得
+    public List<Management> getAllCompletionManagementData() throws DataAccessException;
+
+    // 全未完了データ取得
+    public List<Management> getAllIncompleteManagementData() throws DataAccessException;
+    
+    // idベースで1データを取得
+    public Management getManagementDataById(String id) throws DataAccessException;
+
+    // idベースで1データ更新
+    public int updateManagementData(Management management) throws DataAccessException;
+
+    // delete_atを追加
+    public int deleteManagementDataById(String id) throws DataAccessException;
+
+}
