15855a82896c35bde05c15c71c6a25f521c7d302
kmiibfw <kmiibfw@gmail.com>
2024-11-23 14:47:10 +0900

    [add] 一覧表示ページの作成

---
+++ src/main/java/com/example/team_management/controllers/ManagementController.java
@@ -0,0 +1,44 @@
+package com.example.team_management.controllers;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import com.example.team_management.models.management.Management;
+import com.example.team_management.services.ManagementService;
+import com.example.team_management.services.UserService;
+
+@Controller
+public class ManagementController {
+
+    @Autowired
+    ManagementService managementService;
+
+    @Autowired
+    UserService userService;
+
+
+    @RequestMapping("/")
+    public String tableview(Model model){
+
+        // top page
+        return "index";
+    }
+
+    @RequestMapping("/management")
+    public String managamentView(Model model){
+        List<Management> incomplateList = managementService.getAllIncompleteManagementData();
+        List<Management> complateList = managementService.getAllCompletionManagementData();
+        Map<String, String> userMap = userService.getUserNameList();
+
+
+        model.addAttribute("incomplate_list", incomplateList);
+        model.addAttribute("complate_list", complateList);
+        model.addAttribute("user_list", userMap);
+
+        return "management/management";
+    }
+}
