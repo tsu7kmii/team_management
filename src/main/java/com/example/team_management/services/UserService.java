7923a7c56f81a2f52a0f66bb5b6e305c64722237
kmiibfw <kmiibfw@gmail.com>
2024-11-23 18:26:09 +0900

    [add] ユーザー登録のサービス作成

--- src/main/java/com/example/team_management/services/UserService.java
+++ src/main/java/com/example/team_management/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.team_management.services;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.team_management.models.user.User;
 import com.example.team_management.models.user.UserDao;
 
 @Transactional // トランザクション
@@ -17,9 +20,28 @@ public class UserService {
     @Qualifier("UserDaoJdbc")
     UserDao dao;
 
-    public Map<String, String> getUserNameList(){
+    public Map<Integer, String> getUserNameList(){
         
         // 全ユーザー情報取得
         return dao.getUserNameList();
+    }
+
+    public boolean newUserRegister(User user){
+
+        int rowNumber = dao.newUserRegister(user);
+
+        boolean result = false;
+
+        if (rowNumber > 0) {
+            // 成功
+            result = true;
+        }
+        return result; 
+    }
+
+    public LocalDateTime localDateTimeFormatter(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime accessExpiresAt = LocalDateTime.parse(date, formatter);
+        return accessExpiresAt;
     }
 }
