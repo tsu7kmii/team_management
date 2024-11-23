ac4df854ef42b6603e41af15fb7d03e3de261d2c
kmiibfw <kmiibfw@gmail.com>
2024-11-23 13:42:09 +0900

    [add] userテーブルのサービス：全ユーザー情報取得　を作成

---
+++ src/main/java/com/example/team_management/services/UserServise.java
@@ -0,0 +1,25 @@
+package com.example.team_management.services;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.team_management.models.user.UserDao;
+
+@Transactional // トランザクション
+@Service
+public class UserServise {
+
+    @Autowired
+    @Qualifier("UserDaoJdbc")
+    UserDao dao;
+
+    public Map<String, String> getUserNameList(){
+        
+        // 全ユーザー情報取得
+        return dao.getUserNameList();
+    }
+}
