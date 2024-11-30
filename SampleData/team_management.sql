830a18368a6d1d0ad1ab0d8ab3e6ae296fdf628c
kmiibfw <kmiibfw@gmail.com>
2024-11-30 22:55:26 +0900

    [add] パスワードリセットに必要なテーブル追加

--- SampleData/team_management.sql
+++ SampleData/team_management.sql
@@ -89,6 +89,37 @@ INSERT INTO `management` (`management_id`, `user_id`, `subject`, `link`, `status
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `password_reset_token`
+--
+
+CREATE TABLE `password_reset_token` (
+  `id` int NOT NULL,
+  `token` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` int NOT NULL,
+  `expiry_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `password_reset_token_seq`
+--
+
+CREATE TABLE `password_reset_token_seq` (
+  `id` int NOT NULL,
+  `next_val` bigint NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `password_reset_token_seq`
+--
+
+INSERT INTO `password_reset_token_seq` (`id`, `next_val`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `user_table`
 --
 
@@ -127,6 +158,12 @@ ALTER TABLE `management`
   ADD PRIMARY KEY (`management_id`);
 
 --
+-- テーブルのインデックス `password_reset_token_seq`
+--
+ALTER TABLE `password_reset_token_seq`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- テーブルのインデックス `user_table`
 --
 ALTER TABLE `user_table`
@@ -142,6 +179,12 @@ ALTER TABLE `user_table`
 --
 ALTER TABLE `management`
   MODIFY `management_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- テーブルの AUTO_INCREMENT `password_reset_token_seq`
+--
+ALTER TABLE `password_reset_token_seq`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- テーブルの AUTO_INCREMENT `user_table`
