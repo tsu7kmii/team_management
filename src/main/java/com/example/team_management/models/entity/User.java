06959dc2b8c64cc2ea34af65bb517781d93dfae2
kmiibfw <kmiibfw@gmail.com>
2024-11-30 22:22:42 +0900

    [update] パスワードリセット用 jpa entityの追加

--- src/main/java/com/example/team_management/models/entity/User.java
+++ src/main/java/com/example/team_management/models/entity/User.java
@@ -2,10 +2,16 @@ package com.example.team_management.models.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "user_table")
 @Data
 public class User {
+    @Id
     private int user_id;
     private String user_name;
     private String password;
